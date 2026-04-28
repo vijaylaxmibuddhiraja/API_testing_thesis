f50ce4fab410b3dee8ecc1d3235b66508f0fb1a9
Vijaylaxmi Buddhiraja <vijaylaxmi.buddhiraja@gmail.com>
2026-04-28 18:17:57 +0200

    TC-01 failed, shows 404 status

--- src/test/java/org/example/JsonPlaceholderTests.java
+++ src/test/java/org/example/JsonPlaceholderTests.java
@@ -1,4 +1,34 @@
 package org.example;
 
+import groovyjarjarantlr4.v4.codegen.model.decl.ContextRuleGetterDecl;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matcher.*;
+import static org.hamcrest.Matchers.equalTo;
+
 public class JsonPlaceholderTests {
+
+    @BeforeAll
+    static void setup() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    }
+
+    // TC-01 GET single resource
+    @Test
+    void shouldReturnPostWhenValidProvided() {
+        given()
+                .when()
+                .get("/posts/1")
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(1))
+                .body("userId", equalTo(1));
+    }
+
 }
